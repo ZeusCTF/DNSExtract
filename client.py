018ffe1ea84d040efcf2c77fec2686585822728f
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-03-24 17:58:50 -0400

    progress

--- client.py
+++ client.py
@@ -1,3 +1,6 @@
+#Information is being gleaned from the below notable sources:
+#https://unit42.paloaltonetworks.com/dns-tunneling-how-dns-can-be-abused-by-malicious-actors/, https://techwithrohit.medium.com/dns-exfiltration-what-and-how-of-it-dc2dd70f0337
+
 from scapy.all import *
 
 ans = sr1(IP(dst="8.8.8.8")/UDP(sport=RandShort(), dport=53)/DNS(rd=1,qd=DNSQR(qname="secdev.org",qtype="A")))
